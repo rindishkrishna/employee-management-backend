348ef2a9a8735611d3c19d327d09442dd763a3ad
rindish krishna <rindishkrishna@gmail.com>
2021-01-22 14:36:34 +0530

    cors added

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app =express();
 const logger =require('./Startup/Logging');
+const cors= require('cors');
 require('./Startup/Config')();
 require('./Startup/Prod')(app);
 require('./Startup/Db')();
 require('./Startup/Router')(app);
-
+app.use(cors());
 const port = process.env.PORT;
 app.listen(port,()=>{
 logger.info(`listening on ${port}`)
