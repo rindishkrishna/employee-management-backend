2d8d0d6d3e371485d7219d1a235f67872e0fc2c0
rindish krishna <rindishkrishna@gmail.com>
2021-01-22 14:59:32 +0530

    cors fix

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ require('./Startup/Db')();
 require('./Startup/Router')(app);
 app.use(cors());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://asdl-miniproject.herokuapp.com/"); 
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
